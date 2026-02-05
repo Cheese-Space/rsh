54b0ea2caad81d1b948eabad24413af49725e9b9
Cheese-Space <batumete1011@gmail.com>
2026-02-05 18:44:38 +0100

    added cstring null byte error

--- src/status.rs
+++ src/status.rs
@@ -3,14 +3,16 @@ pub type ShellResult = Result<Returns, ShellError>;
 pub enum ShellError {
     Fork(nix::errno::Errno),
     IO(nix::errno::Errno),
-    NoArg
+    NoArg,
+    CStringNullByte
 }
 impl std::fmt::Display for ShellError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             ShellError::Fork(error) => write!(f, "error: {}", error.desc()),
             ShellError::IO(error) => write!(f, "error: {}", error.desc()),
-            ShellError::NoArg => write!(f, "error: no argument provided!")
+            ShellError::NoArg => write!(f, "error: no argument(s) provided!"),
+            ShellError::CStringNullByte => write!(f, "error: null byte found before end of string")
         }
     }
 }
