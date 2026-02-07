0e1d5ed379deea2fa002744779d276b3f5b1bd17
Cheese-Space <batumete1011@gmail.com>
2026-02-07 13:41:17 +0100

    add exec error

--- src/status.rs
+++ src/status.rs
@@ -3,10 +3,11 @@ pub type ShellResult = Result<Returns, ShellError>;
 pub enum ShellError {
     Fork(nix::errno::Errno),
     IO(nix::errno::Errno),
-    NoArg,
-    CStringNullByte,
+    Exec(nix::errno::Errno),
     DupStdout(nix::errno::Errno, String),
-    DupStdin(nix::errno::Errno, String)
+    DupStdin(nix::errno::Errno, String),
+    NoArg,
+    CStringNullByte
 }
 impl std::fmt::Display for ShellError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -16,7 +17,8 @@ impl std::fmt::Display for ShellError {
             ShellError::NoArg => write!(f, "error: no argument(s) provided!"),
             ShellError::CStringNullByte => write!(f, "error: null byte found before end of string"),
             ShellError::DupStdout(error, filename) => write!(f, "error: {}\nfailed to redirect stdout to '{}'", error.desc(), filename),
-            ShellError::DupStdin(error, filename) => write!(f, "error: {}\nfailed to redirect stdin to '{}'", error.desc(), filename)
+            ShellError::DupStdin(error, filename) => write!(f, "error: {}\nfailed to redirect stdin to '{}'", error.desc(), filename),
+            ShellError::Exec(error) => write!(f, "error: {}", error.desc())
         }
     }
 }
