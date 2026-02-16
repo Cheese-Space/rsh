25fa41d6f1154228ce7d05b270204d2c604a891a
Cheese-Space <batumete1011@gmail.com>
2026-02-16 22:23:40 +0100

    removed parse_input() (will re-add it later?)

--- src/parse.rs
+++ src/parse.rs
@@ -1,11 +1,5 @@
-use std::io;
 use std::ffi::CString;
 use crate::status::ShellError;
-pub fn parse_input() -> String {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    input
-} 
 pub fn split_input(input: &str) -> Result<Vec<CString>, ShellError> {
     let input = input.split_whitespace();
     let mut c_input: Vec<CString> = Vec::new();
